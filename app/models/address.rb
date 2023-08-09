8441bd5e5a5801bcfadcecaa74f5c5aaf114be6a
Ueverton-oliveira <ueverton.souz@gmail.com>
2023-08-09 16:30:21 -0400

    fix: FaeV2 Menus to Menu School

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,13 @@
+# frozen_string_literal: true
+
 class Address < ApplicationRecord
+  # association
+  # --
+
   belongs_to :user
+
+  # validation
+  # --
+
   validates :name, presence: true
 end
